3ce1a3d39264c801f8033e275759f6e96228fac1
palaciosmegan <mpalacios@grupofriopacking.pe>
2026-06-09 09:18:54 -0500

    fixing getDisplayName function

--- src/ui/Calibrador.tsx
+++ src/ui/Calibrador.tsx
@@ -12,7 +12,7 @@ const orientationParsed = { INT: 'Interior', EXT: 'Exterior' }
 const getDisplayName = (sensor: SensorMock) => {
 	const sensorName = sensor.id.substring(0, 1) === 'T' ? sensor.id.split(' ')[1] : sensor.id
 	if (sensorName.substring(0, 1) === 'S') {
-		return sensorName.split('0').join('') + ' - ' + orientationParsed[sensor.orientation]
+		return 'S' + sensorName.split('S')[1].replace(/^0+(?!$)/, '') + ' - ' + orientationParsed[sensor.orientation]
 	}
 	return 'Sensor Ambiente'
 }
